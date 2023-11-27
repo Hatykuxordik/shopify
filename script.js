f1f87974392ac10ca9e65968fd28e56ca7deefa5
Sodiq Atiku <hatykuxordik@gmail.com>
2023-11-27 15:58:54 +0100

    Notification Popup Update

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const guideContent = document.querySelector(".guide__content");
 const listItems = document.querySelectorAll(".list-item");
 const planPopup = document.querySelector(".plan-popup");
 const closeBtn = document.querySelector(".btn-close");
+const popup = document.querySelector(".popup");
+const notificationBell = document.querySelector(".notification-bell");
+const container = document.querySelector(".container");
 
 document.addEventListener("DOMContentLoaded", function () {
   // Open the first list item by default
@@ -32,6 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   listItems.forEach((item) => {
     item.addEventListener("click", () => {
+      // Check if the clicked item is already active
+      const isActive = item.classList.contains("active");
+
       listItems.forEach((otherItem) => {
         if (otherItem !== item) {
           otherItem.classList.remove("active");
@@ -40,19 +46,26 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
 
-      item.classList.add("active");
-
-      const hiddenBox = item.querySelector(".hidden-box");
-
-      if (hiddenBox.style.display === "none") {
+      // If the clicked item is not already active, make it active and show the hidden box
+      if (!isActive) {
+        item.classList.add("active");
+        const hiddenBox = item.querySelector(".hidden-box");
         hiddenBox.style.display = "flex";
-      } else {
-        hiddenBox.style.display = "none";
       }
     });
   });
 
   closeBtn.addEventListener("click", () => {
     planPopup.style.visibility = "hidden";
+  });
+
+  notificationBell.addEventListener("click", () => {
+    popup.classList.toggle("visible");
+  });
+
+  container.addEventListener("click", function (event) {
+    if (popup.classList.contains("visible") && event.target === container) {
+      popup.classList.remove("visible");
+    }
   });
 });
